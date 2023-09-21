424547e2e37db05fc2b9eb5fc3add95c6a73dfa8
pietervdvn <pietervdvn@posteo.net>
2023-09-21 14:48:22 +0000

    Deploying a new version

--- service-worker.js
+++ service-worker.js
@@ -35,7 +35,7 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
     }
 };
 var _this = this;
-var version = "0.0.8-e6d84019a579daed01a34cf67825689511fd9c0a";
+var version = "0.0.8-865b0bc44f694ad9c2295466fbe4c8abea266386";
 function install() {
     return __awaiter(this, void 0, void 0, function () {
         return __generator(this, function (_a) {
