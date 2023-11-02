2ed15c5580e815a73fbca987c9fa0e8dfe586b24
pietervdvn <pietervdvn@posteo.net>
2023-11-02 03:57:30 +0000

    Deploying a new version

---
+++ assets/land-77847272.js
@@ -0,0 +1,2 @@
+import"./Utils-48ded42e.js";import{O as i}from"./OsmConnection-21906511.js";import"./_commonjsHelpers-87174ba5.js";import"./LocalStorageSource-5c82bae4.js";import"./Constants-0efc4b02.js";console.log("Authorizing...");new i().finishLogin(o=>{const n=window.location.protocol+"//"+window.location.host+"/index.html";o??(o=n),o.indexOf("/land")>0&&(o=n),console.log("Redirecting to",o),window.location.href=o});
+//# sourceMappingURL=land-77847272.js.map
