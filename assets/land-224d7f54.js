8cfe7fcfdc56bae2a86136d6de270e6e9f0d552b
pietervdvn <pietervdvn@posteo.net>
2024-03-05 00:05:17 +0000

    Deploying a new version

---
+++ assets/land-224d7f54.js
@@ -0,0 +1,2 @@
+import"./Utils-45e0ac39.js";import{O as i}from"./OsmConnection-419e8a8f.js";import"./_commonjsHelpers-de833af9.js";import"./UIEventSource-b2e0ae35.js";import"./LocalStorageSource-152a04ab.js";import"./Constants-66b06235.js";console.log("Authorizing...");new i().finishLogin(o=>{const n=window.location.protocol+"//"+window.location.host+"/index.html";o??(o=n),o.indexOf("/land")>0&&(o=n),console.log("Redirecting to",o),window.location.href=o});
+//# sourceMappingURL=land-224d7f54.js.map
--- service-worker.js
+++ service-worker.js
@@ -35,7 +35,7 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
     }
 };
 var _this = this;
-var version = "0.0.8-99af2f31b6d28449983875ed02fd89278f70baf5";
+var version = "0.0.8-62c6e20a37a9cbecff260b5da35f6503dd3f828c";
 function install() {
     return __awaiter(this, void 0, void 0, function () {
         return __generator(this, function (_a) {
