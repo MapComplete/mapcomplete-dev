56710ce5de62a463cd375563469048b7ee749a2d
pietervdvn <pietervdvn@posteo.net>
2024-03-19 01:10:35 +0000

    Deploying a new version

---
+++ assets/test-547e9d84.js
@@ -0,0 +1,2 @@
+import"./Utils-45e0ac39.js";import{S as o,i,s as r,O as c,d as l,n as s,h as m,k as p}from"./index-8d610534.js";function u(n){let t;return{c(){t=c("No tests")},m(e,a){l(e,t,a)},p:s,i:s,o:s,d(e){e&&m(t)}}}class d extends o{constructor(t){super(),i(this,t,null,u,r,{})}}new p(d).AttachTo("maindiv");
+//# sourceMappingURL=test-547e9d84.js.map
--- service-worker.js
+++ service-worker.js
@@ -35,7 +35,7 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
     }
 };
 var _this = this;
-var version = "0.0.8-4467240224084231a7fc6a7dcf6f516966c917d2";
+var version = "0.0.8-3daac428533ec339869599366b7d58e9831b2dde";
 function install() {
     return __awaiter(this, void 0, void 0, function () {
         return __generator(this, function (_a) {
